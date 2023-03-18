ac983225ffd87f75f1c49ca1500905ebdd17c477
dja1716 <hdum@uwaterloo.ca>
2023-03-18 16:54:47 +0900

    Create formUseState.js

---
+++ FrontEnd/React/formUseState.js
@@ -0,0 +1,76 @@
+function App() {
+  const [user, setUser] = useState({
+    name: "",
+    surname: "",
+    username: "",
+    email: "",
+    password: "",
+    country: "",
+    city: "",
+    address: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  console.log(user);
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          placeholder="name"
+        />
+        <input
+          type="text"
+          name="surname"
+          onChange={handleChange}
+          placeholder="surname"
+        />
+        <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          placeholder="username"
+        />
+        <input
+          type="text"
+          name="email"
+          onChange={handleChange}
+          placeholder="email"
+        />
+        <input
+          type="text"
+          name="password"
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <input
+          type="text"
+          name="country"
+          onChange={handleChange}
+          placeholder="country"
+        />
+        <input
+          type="text"
+          name="city"
+          onChange={handleChange}
+          placeholder="city"
+        />
+        <input
+          type="text"
+          name="address"
+          onChange={handleChange}
+          placeholder="address"
+        />
+      </form>
+    </div>
+  );
+}
