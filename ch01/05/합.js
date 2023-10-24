4982b777dc4d96ef2398bef45e419b2add5cffcd
dja1716 <dja12356@gmail.com>
2023-10-24 22:18:15 +0900

    Create 합.js

---
+++ ch01/05/합.js
@@ -0,0 +1,11 @@
+// 10:15
+let fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let n = Number(input[0]);
+let answer = 0;
+for(let i = 1; i <= n; i++) {
+    answer += i;
+}
+
+console.log(answer);
