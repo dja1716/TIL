b280eb0f369f9780e4b29faa82c2125dd0897c15
dja1716 <dja12356@gmail.com>
2023-10-24 20:53:38 +0900

    Update 0.3A*B.js

--- ch01/0.3A*B.js
+++ ch01/0.3A*B.js
@@ -2,6 +2,6 @@ let fs = require('fs');
 
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let answer = input[0].split(' ')[0] * input[0].split(' ')[1];
+let answer = Number(input[0].split(' ')[0]) * Number(input[0].split(' ')[1]);
 
 console.log(answer);
