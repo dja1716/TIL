688c525a1765127d89d7b861ee50868af89fe492
dja1716 <dja12356@gmail.com>
2023-10-25 23:02:41 +0900

    Create 주몽.java

---
+++ Java/주몽.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(bf.readLine());
+        long target = Long.parseLong(st.nextToken());
+        int[] materials = new int[n];
+        st = new StringTokenizer(bf.readLine());
+        
+        for(int i = 0; i < n; i++) {
+            materials[i] = Integer.parseInt(st.nextToken());
+        }
+        int count = 0;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(materials[i] + materials[j] == target) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
