3d31aabb151864d889f95f741fcc4bbb9af72ddd
dja1716 <dja12356@gmail.com>
2023-10-25 22:43:21 +0900

    Create 수들의합5.java

---
+++ Java/수들의합5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        
+        long count = 1;
+        long start = 1;
+        long end = 1;
+        long sum = 1;
+        
+        while(end < n) {
+            if(sum == n) {
+                count++;
+                end++;
+                sum += end;
+            } else if(sum < n) {
+                end++;
+                sum += end;
+            } else if(sum > n) {
+                sum -= start;
+                start++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
