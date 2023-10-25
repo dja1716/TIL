42c8aba0833f37503561a4a593d1a0b70f7035bc
dja1716 <dja12356@gmail.com>
2023-10-25 15:37:04 +0900

    Create 최댓값.java

---
+++ Java/최댓값.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int maxValue = 0;
+        int order =0;
+        
+        for(int i = 0; i < 9; i++) {
+            int temp = sc.nextInt();
+            if(temp > maxValue) {
+                maxValue = temp;
+                order = i + 1;
+            }
+        }
+        System.out.println(maxValue);
+        System.out.println(order);
+    }
+}
