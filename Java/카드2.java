87a1b9a9be251c6ae2969370313ebfbb31c704f7
dja1716 <dja12356@gmail.com>
2023-10-27 15:31:17 +0900

    Create 카드2.java

---
+++ Java/카드2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> deck = new LinkedList();
+
+        int maxN = sc.nextInt();
+
+        for(int i = 1; i <= maxN; i++) {
+            deck.add(i);
+        }
+
+        while(maxN != 1) {
+            // till one card left;
+            deck.poll();
+            maxN--;
+            deck.add(deck.poll());
+        }
+
+        // print
+        System.out.println(deck.peek());
+
+        sc.close();
+    }
+}
