d35cfb4b04dfae6383cdc8424b0be17c1116d122
dja1716 <dja12356@gmail.com>
2023-10-31 17:37:06 +0900

    Create 단어정렬.java

---
+++ fast_campus/java/01/Sort/단어정렬.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        String[] arr = new String[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = bf.readLine();
+        }
+
+        Arrays.sort(arr, (str1, str2) -> {
+            if(str1.length() == str2.length()) {
+                return str1.compareTo(str2);
+            }
+            return str1.length() - str2.length();
+        });
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i = 0; i < N; i++) {
+            if(i > 0 && arr[i].equals(arr[i-1])) continue;
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+    }
+}
