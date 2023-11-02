9412cfe90035191f41cb791b5b0868eea3fb9690
dja1716 <dja12356@gmail.com>
2023-11-02 16:15:47 +0900

    Create 국영수.java

---
+++ fast_campus/java/05/Sorting/국영수.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static class Student implements Comparable<Student> {
+        public String name;
+        public int korean, math, english;
+        
+        @Override
+        public int compareTo(Student other) {
+            if(korean != other.korean) {
+                return other.korean - korean;
+            }
+            if(english != other.english) {
+                return english - other.english;
+            }
+            if(math != other.math) {
+                return other.math - math;
+            }
+            return name.compareTo(other.name);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        Student[] list = new Student[N];
+        
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String name = st.nextToken();
+            int korean = Integer.parseInt(st.nextToken());
+            int english = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
+            Student newStudent = new Student();
+            newStudent.name = name;
+            newStudent.english = english;
+            newStudent.korean = korean;
+            newStudent.math = math;
+            list[i] = newStudent;
+        }
+        
+        Arrays.sort(list);
+        
+        for(int i = 0; i < N; i++) {
+            bw.write(list[i].name + "\n");
+        }
+        
+        bw.flush();
+    }
+}
