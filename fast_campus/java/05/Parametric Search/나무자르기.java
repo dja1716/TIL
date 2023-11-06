e2ca82e064f88bdb7ce4a8f675f224b72516d7e5
dja1716 <dja12356@gmail.com>
2023-11-06 20:37:44 +0900

    Create 나무자르기.java

---
+++ fast_campus/java/05/Parametric Search/나무자르기.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] trees;
+    static int N;
+    static int M;
+    static BufferedReader bf;
+    static int treeMaxHeight = -1;
+    
+    
+    public static void main(String[] args) throws IOException {
+        getInput();
+        process();
+    }
+    
+    public static void getInput() throws IOException {
+        bf = new BufferedReader (new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        trees = new int[N];
+        
+        st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            if(trees[i] > treeMaxHeight) {
+                treeMaxHeight = trees[i];
+            }
+        }
+    }
+    
+    public static void process() {
+        int left = 0;
+        int right = treeMaxHeight;
+        int result = 0;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            long temp = getTreesCut(mid);
+            
+            if(temp >= M) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(result);
+    }
+    
+    
+    public static long getTreesCut(int height) {
+        long result = 0;
+        for(int i = 0; i < N; i++) {
+            if(trees[i] > height) {
+                result += (long) (trees[i] - height);
+            }
+        }
+        
+        return result;
+    }
+}
