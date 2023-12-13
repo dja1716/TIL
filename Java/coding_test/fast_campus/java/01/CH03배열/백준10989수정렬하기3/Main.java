f2f0471f343e025f7e51ea0cde74db6a498542a5
Hyeon Um <dja12356@gmail.com>
2023-12-13 21:20:32 +0900

    another coding problem

---
+++ Java/coding_test/fast_campus/java/01/CH03배열/백준10989수정렬하기3/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static int[] arr = new int[10001];
+  public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int N = Integer.parseInt(st.nextToken());
+
+      for(int i = 0; i < N; i++) {
+          st = new StringTokenizer(br.readLine());
+          arr[Integer.parseInt(st.nextToken())]++;
+      }
+
+
+
+      for(int i = 1; i < arr.length; i++) {
+          for(int j = 0; j < arr[i]; j++) {
+              bw.write(i + "\n");
+          }
+      }
+      bw.close();
+  }
+}
