3539eba2b49ff41141483cd41b3f309a41499a16
Hyeon Um <dja12356@gmail.com>
2023-12-13 22:33:18 +0900

    solved

---
+++ Java/coding_test/fast_campus/java/01/CH03배열/백준3273두수의합/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static boolean[] arr = new boolean[1000001];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+
+        String[] arrS = br.readLine().split(" ");
+
+        for(int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(arrS[i]);
+            arr[temp] = true;
+        }
+        st = new StringTokenizer(br.readLine());
+        int target = Integer.parseInt(st.nextToken());
+        int untill = target / 2;
+        int answer = 0;
+
+        for(int i = 1; i <= untill; i++) {
+            int counterPart = target - i;
+            if(counterPart <= 1000000 && arr[i] && arr[target - i] && i  != counterPart) {
+                answer++;
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+        bw.close();
+    }
+}
