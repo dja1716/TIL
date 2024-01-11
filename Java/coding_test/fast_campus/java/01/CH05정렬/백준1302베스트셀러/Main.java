b88b155c4b0e55d585162e65734fd445ac66a162
Hyeon Um <dja12356@gmail.com>
2024-01-11 12:44:18 +0900

    solved

---
+++ Java/coding_test/fast_campus/java/01/CH05정렬/백준1302베스트셀러/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static String answer;
+    static int curCount;
+    static Map<String, Integer> books = new HashMap();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bf.readLine());
+        String bookTemp = st.nextToken();
+        books.put(bookTemp, 1);
+        curCount = 1;
+        answer = bookTemp;
+
+        for(int i = 1; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String bookTemp2 = st.nextToken();
+            Integer tempCount = 1;
+            if(books.containsKey(bookTemp2)) {
+                tempCount = books.get(bookTemp2) + 1;
+            }
+            
+            books.put(bookTemp2, tempCount);
+            if(curCount < tempCount) {
+                answer = bookTemp2;
+                curCount = tempCount;
+            } else if(curCount == tempCount && answer.compareTo(bookTemp2) > 0) {
+                answer = bookTemp2;
+                curCount = tempCount;
+            }
+        }
+
+
+        bw.write(answer);
+        bw.flush();
+    }
+
+}
