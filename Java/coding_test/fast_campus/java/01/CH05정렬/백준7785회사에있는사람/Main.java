3c8b681904104dc60fdd7cb5eadb8796a537872a
Hyeon Um <dja12356@gmail.com>
2024-01-11 11:31:01 +0900

    solved

---
+++ Java/coding_test/fast_campus/java/01/CH05정렬/백준7785회사에있는사람/Main.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static Set<String> log = new HashSet();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            String name = st.nextToken();
+            String action = st.nextToken();
+
+            if(action.equals("enter")) {
+                log.add(name);
+            } else {
+                log.remove(name);
+            }
+        }
+        List<String> answer = new ArrayList<>(log);
+
+            Comparator<String> customComparator = new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o2.compareTo(o1);
+                }
+            };
+
+            Collections.sort(answer, customComparator);
+            
+            for(String name : answer) {
+                bw.write(name + '\n');
+            }
+
+        bw.flush();
+    }
+
+}
