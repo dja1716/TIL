e7e60af91aa8ccf08c00141bc96421b6d6158b64
Hyeon Um <dja12356@gmail.com>
2024-05-03 14:27:03 +0900

    solved

---
+++ Java/coding_test/programmers/대표유형/수학/두원사이의정수쌍/Solution.java
@@ -0,0 +1,23 @@
+package Java.coding_test.programmers.대표유형.수학.두원사이의정수쌍;
+
+import java.util.*;
+
+class Solution {
+    public long solution(int r1, int r2) {
+        long outer = (long) r2 * (long) r2;
+        long inner = (long) r1 * (long) r1;
+        long answer = 0;
+        long[] arr = new long[r2 + 1];
+        for(long a = 1; a <= r2; a++) {
+            long o = (long) Math.floor(Math.sqrt(outer - a * a));
+            long i = (long) Math.ceil(Math.sqrt(inner - a * a));
+            // System.out.println("a: " + a + " o: " + o + " i: " + i);
+            arr[(int)a] = o - i + 1;
+        }
+        
+        for(long ans : arr) {
+            answer += ans;
+        }
+        return answer * 4;
+    }
+}
